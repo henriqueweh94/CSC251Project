b4961643d75176c1ea77493fbc8dd57cd8548b89
henriqueweh94 <hweh0000@email.cpcc.edu>
2023-02-27 21:10:55 -0500

    Modified to read input from a file

--- Project_henrique_weh.java
+++ Project_henrique_weh.java
@@ -5,42 +5,40 @@
 */
 
 import java.util.Scanner;//Import for the Scanner class to read keyboard inputs
+import java.util.ArrayList;//Import of the ArrayList class
+import java.io.*;//Import used to read/write to a file
 
 public class Project_henrique_weh
 {
-   public static void main(String args[])
+   public static void main(String args[]) throws IOException
    {  
       //variable declarations
       int num, age;
       String provider, fName, lName, smokeStatus;
       double height, weight;
       
-      Scanner keyboard = new Scanner(System.in);//variable to read keyboard inputs
+     // Scanner keyboard = new Scanner(System.in);//variable to read keyboard inputs
+     
+      File policyinfo = new File("PolicyInformation.txt");//creating a file object
+      Scanner read = new Scanner(policyinfo);//creatint a scanner object to read the file
       
-      //User Prompts for input
-      System.out.print("Please enter the Policy Number: ");
-      num = keyboard.nextInt();
-      keyboard.nextLine();
-      System.out.print("Please enter the Provider Name: ");
-      provider = keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's First Name: ");
-      fName = keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's Last Name: ");
-      lName = keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's Age: ");
-      age = keyboard.nextInt();
-      keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's Smoking Status: ");
-      smokeStatus = keyboard.nextLine();
-      System.out.print("Please enter the Policyholder's Height: ");
-      height = keyboard.nextDouble();
-      System.out.print("Please enter the Policyholder's Weight: ");
-      weight = keyboard.nextDouble();
+      ArrayList<Policy> policies = new ArrayList<Policy>();
       
-      //Creating a object of the Policy class called P1
-      Policy p1 = new Policy(num, provider, fName, lName, age, smokeStatus, height, weight);
-      
-      System.out.println(p1.getInfo());
-      
+      while(read.hasNext())
+      {
+         num = read.nextInt();
+         read.nextLine();
+         provider = read.nextLine();
+         fName = read.nextLine();
+         lName = read.nextLine();
+         age = read.nextInt();
+         read.nextLine();
+         smokeStatus = read.nextLine();
+         height = read.nextDouble();
+         weight = read.nextDouble();
+         
+         policies.add(new Policy(num, provider, fName, lName, age, smokeStatus, height, weight));
+      }
+      read.close();
    }
 }
